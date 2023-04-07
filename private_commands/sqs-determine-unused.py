39220b3a0c30a48cc0a883ff6ace4cb7ce7ac5c7
Sebastian Alvis <salvis@hearsaycorp.com>
2023-04-07 10:09:06 -0700

    sqs-determine-unused now fetches metrics for one queue and saves the results back to disk

--- private_commands/sqs-determine-unused.py
+++ private_commands/sqs-determine-unused.py
@@ -8,6 +8,7 @@ from shared.nodes import Account, Region
 from shared.common import query_aws, get_regions, parse_arguments, get_parameter_file
 from datetime import datetime, date, timedelta
 from shared.common import get_account, custom_serializer
+from commands.collect import get_filename_from_parameter
 from botocore.exceptions import ClientError, EndpointConnectionError, NoCredentialsError
 
 def make_directory(path):
@@ -86,23 +87,47 @@ def get_sqs_queue_metrics(arguments, accounts, config):
     #response = client.get_metric_data(**parameters)
     #print('Metric Total: ', sum(response['MetricDataResults'][0]['Values']))
 
+    # parameter file is loaded into saved_sqs_list
+    sqs_dynamic_param = "QueueUrl"
+
     for account in accounts:
         for region_json in get_regions(Account(None, account)):
             region = Region(Account(None, account), region_json)
             print(f"Region: {region.name}")
             saved_sqs_list = query_aws(region.account, "sqs-list-queues", region=region)
-            #print(saved_sqs_list.keys())
-            if 'QueueUrls' in saved_sqs_list:
-                print(saved_sqs_list['QueueUrls'][0])
+            sqs_attributes_filepath = "account-data/{}/{}/{}-{}".format(
+                arguments.profile, region.name, "sqs", "get-queue-attributes"
+            )
 
-                saved_sqs_details = get_parameter_file(region, "sqs", "get-queue-attributes", saved_sqs_list['QueueUrls'][0])
+            if 'QueueUrls' in saved_sqs_list:
+                #print(saved_sqs_list['QueueUrls'][0])
+                queue_url = saved_sqs_list['QueueUrls'][0]
+                saved_sqs_details = get_parameter_file(region, "sqs", "get-queue-attributes", queue_url)
                 print(saved_sqs_details)
+
+                queue_arn = saved_sqs_details['Attributes']['QueueArn']
                 queue_name = saved_sqs_details['Attributes']['QueueArn'].split(':')[-1]
                 print(f"Queue Name: {queue_name}")
 
-                parameters['MetricDataQueries'][0]['MetricStat']['Metric']['Dimensions'][0]['Value'] = queue_name
-                response = client.get_metric_data(**parameters)
-                print('Metric Total: ', sum(response['MetricDataResults'][0]['Values']))
+                if "MessagesReceivedInLastMonth" not in saved_sqs_details['Attributes']:
+                    parameters['MetricDataQueries'][0]['MetricStat']['Metric']['Dimensions'][0]['Value'] = queue_name
+                    response = client.get_metric_data(**parameters)
+                    messages_received = sum(response['MetricDataResults'][0]['Values'])
+                    print('Messages Received in Last Month: ', messages_received)
+                    saved_sqs_details['Attributes']['MessagesReceivedInLastMonth'] = messages_received
+
+                    # This is the filename we want to save the data to
+                    #print(get_filename_from_parameter(queue_url))
+                    filename = get_filename_from_parameter(queue_url)
+                    outputfile = "{}/{}".format(sqs_attributes_filepath, filename)
+                    print(f"Output File: {outputfile}")
+                    if saved_sqs_details is not None:
+                        with open(outputfile, "w+") as f:
+                            f.write(
+                                json.dumps(saved_sqs_details, indent=4, sort_keys=True, default=custom_serializer)
+                            )
+
+
 
 def run(arguments):
 
